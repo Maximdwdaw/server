1eb4d306c71a61e51e12c9238fed738d3085e441
Maximdwdaw <142977598+Maximdwdaw@users.noreply.github.com>
2023-11-08 13:09:00 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://blog-three-mauve.vercel.app/');
   next();
 });
 
